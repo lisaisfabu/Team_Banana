5d258461c2fe7699119cf1bfdb8647f6455b5975
Huong Dang <huongd97@gmail.com>
2018-01-27 21:01:39 -0500

    Updated so that food can be displayed

--- Maze_Game.py
+++ Maze_Game.py
@@ -1,6 +1,6 @@
 from pygame.locals import *
 import pygame
- 
+
 class Player:
     x = 57
     y = 57
@@ -19,29 +19,15 @@ class Player:
         self.y = self.y + self.speed
  
 class Maze:
+
     def __init__(self):
        self.M = 12
        self.N = 12
        
-       '''
-       self.maze = [ 1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-                     1,0,0,0,0,0,0,0,0,0,1,1,0,1,
-                     1,0,1,1,1,0,1,1,1,0,1,0,0,1,
-                     1,0,1,0,0,0,0,0,0,0,0,0,1,1,
-                     1,0,1,0,0,0,0,0,0,0,1,0,0,1,
-                     1,0,0,0,0,0,0,0,0,1,1,0,1,1,
-                     1,0,1,1,1,0,1,1,0,0,1,1,1,1,
-                     1,0,0,0,0,0,1,1,1,0,0,0,0,1,
-                     1,0,0,1,1,0,0,1,1,0,1,0,0,1,
-                     1,1,0,1,1,1,0,0,1,0,1,0,1,1,
-                     1,0,0,0,0,0,0,0,0,0,1,0,0,1,
-                     1,0,1,0,1,1,1,1,0,1,1,0,1,1,
-                     1,0,0,0,0,1,1,0,0,1,0,0,0,0,
-                     1,1,1,1,1,1,1,0,1,1,1,1,1,1,]
-                     '''
+       
        self.maze = [ 1,1,1,1,1,1,1,1,1,1,1,1,
                      1,0,0,0,0,0,0,0,0,0,0,1,
-                     1,0,1,1,1,0,1,1,1,0,1,1,
+                     1,2,1,1,1,0,1,1,1,0,1,1,
                      1,0,1,0,0,0,0,0,1,0,1,1,
                      1,0,1,0,1,0,0,0,0,1,0,1,
                      1,0,0,0,1,0,1,0,0,0,0,1,
@@ -52,17 +38,70 @@ class Maze:
                      1,0,0,1,0,0,0,0,0,0,0,1,
                      1,1,1,1,1,1,1,1,1,1,0,0,]
  
-    def draw(self,display_surf,image_surf):
+    def draw(self,display_surf):
        bx = 0
        by = 0
        for i in range(0,self.M*self.N):
-           if self.maze[ bx + (by*self.M) ] == 1:
-               display_surf.blit(image_surf,( bx * 57, by * 57))
- 
+           # image = pygame.image.load(self.iron.get(self.maze[ bx + (by*self.M)])).convert()
+            
+           if self.maze[bx + (by*self.M)] == 1:
+               image = pygame.image.load("grassF.png").convert()
+               display_surf.blit(image, (bx*57, by*57))
+
+           elif self.maze[bx + (by*self.M)] == 2:
+               image = pygame.image.load("tofu.png").convert()
+               display_surf.blit(image, (bx*57, by*57))
+
+           elif self.maze[bx + (by*self.M)] == 3:
+               image = pygame.image.load("thyme.png").convert()
+               display_surf.blit(image, (bx*57, by*57))
+
+           elif self.maze[bx + (by*self.M)] == 4:
+               image = pygame.image.load("tea.png").convert()
+               display_surf.blit(image, (bx*57, by*57))
+
+           elif self.maze[bx + (by*self.M)] == 5:
+               image = pygame.image.load("spinach.png").convert()
+               display_surf.blit(image, (bx*57, by*57))
+
+           elif self.maze[bx + (by*self.M)] == 6:
+               image = pygame.image.load("pistachios.png").convert()
+               display_surf.blit(image, (bx*57, by*57))
+
+           elif self.maze[bx + (by*self.M)] == 7:
+               image = pygame.image.load("eggs.png").convert()
+               display_surf.blit(image, (bx*57, by*57))
+
+           elif self.maze[bx + (by*self.M)] == 8:
+               image = pygame.image.load("dairy.png").convert()
+               display_surf.blit(image, (bx*57, by*57))
+
+           elif self.maze[bx + (by*self.M)] == 9:
+               image = pygame.image.load("coffee.png").convert()
+               display_surf.blit(image, (bx*57, by*57))
+
+           elif self.maze[bx + (by*self.M)] == 10:
+               image = pygame.image.load("coconutmilk.png").convert()
+               display_surf.blit(image, (bx*57, by*57))
+
+           elif self.maze[bx + (by*self.M)] == 11:
+               image = pygame.image.load("amaranth.png").convert()
+               display_surf.blit(image, (bx*57, by*57))
+
+
            bx = bx + 1
            if bx > self.M-1:
                bx = 0 
                by = by + 1
+        
+
+            
+
+'''
+    iron = {1: "grassF.png", 2: "tofu.png", 3: "thyme.png", 4: "tea.png",
+            5: "spinach.png", 6: "pistachios.png", 7: "eggs.png", 8: "dairy.png",
+            9: "coffee.png", 10: "coconutmilk.png", 11: "amaranth.png"}
+'''
  
  
 class App:
@@ -98,7 +137,7 @@ class App:
     def on_render(self):
         self._display_surf.fill((255,255,255))
         self._display_surf.blit(self._image_surf,(self.player.x,self.player.y))
-        self.maze.draw(self._display_surf, self._block_surf)
+        self.maze.draw(self._display_surf)
         pygame.display.flip()
  
     def on_cleanup(self):
@@ -107,41 +146,14 @@ class App:
     def on_execute(self):
         if self.on_init() == False:
             self._running = False
- 
-<<<<<<< HEAD
 
-=======
-        # while( self._running ):
-        '''
-            pygame.event.pump()
-            keys = pygame.key.get_pressed()
- 
-            if (keys[K_RIGHT]):
-                self.player.moveRight()
- 
-            if (keys[K_LEFT]):
-                self.player.moveLeft()
- 
-            if (keys[K_UP]):
-                self.player.moveUp()
- 
-            if (keys[K_DOWN]):
-                self.player.moveDown()
-
-            if (keys[QUIT]):
-                self._running = False
- 
-            if (keys[K_ESCAPE]):
-                self._running = False'''
->>>>>>> parent of ed53ed0... push
         while (self._running):
             for event in pygame.event.get():
                 if event.type == QUIT:
                     self._running = False
                 if event.type == KEYDOWN:
                     if event.key == K_DOWN:
-                        if self.maze.maze[(self.player.y + self.player.speed)//57*10+1] != 1:
-                            self.player.moveDown()
+                        self.player.moveDown()
                     if event.key == K_UP:
                         self.player.moveUp()
                     if event.key == K_LEFT:
@@ -158,3 +170,4 @@ class App:
 if __name__ == "__main__" :
     theApp = App()
     theApp.on_execute()
+
